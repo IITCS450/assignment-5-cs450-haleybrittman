bb800c07087617341635bc4528449722a0dc3d29
Haley Brittman <hbrittman@hawk.iit.edu>
2026-03-30 19:44:44 -0500

    implementation for uthread

--- user/uthread.c
+++ user/uthread.c
@@ -3,9 +3,30 @@
 #include "user.h"
 #include "uthread.h"
 
-// TODO: Implement cooperative user-level threads.
 
-void thread_init(void){}
-tid_t thread_create(void (*fn)(void*), void *arg){ (void)fn; (void)arg; return -1; }
+typedef struct thread {
+    tid_t tid;
+    void *stack;
+    void (*fn)(void*);
+    void *arg;
+    struct thread *next;
+} thread_t;
+
+typedef struct thread_queue {
+    thread_t *head;
+    thread_t *tail;
+} thread_queue_t;
+
+
+static thread_queue_t ready_queue;
+static thread_t *current_thread;
+
+void thread_init(void) {
+    ready_queue.head = NULL;
+    ready_queue.tail = NULL;
+    current_thread = NULL;
+}
+
+void thread_create(void (*fn)(void*), void *arg){ (void)fn; (void)arg; return -1; }
 void thread_yield(void){}
 int thread_join(tid_t tid){ (void)tid; return -1; }
