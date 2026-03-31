4b2f0276901f9456ca96a6248ecd229b2600a496
Haley Brittman <hbrittman@hawk.iit.edu>
2026-03-30 20:36:49 -0500

    adding mutex init

--- user/umutex.c
+++ user/umutex.c
@@ -4,6 +4,8 @@
 #include "uthread.h"
 #include "umutex.h"
 
-void mutex_init(umutex_t *m){ m->locked = 0; }
+void mutex_init(umutex_t *m) {
+    m->locked = 0; 
+}
 void mutex_lock(umutex_t *m){ while(m->locked){ thread_yield(); } m->locked = 1; }
 void mutex_unlock(umutex_t *m){ m->locked = 0; }
